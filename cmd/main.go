31c16e4cf2fedabb99255fac619b508fb54feb37
Vitalik Boriskin <gkeyfrost-11@yandex.ru>
2025-12-04 13:54:53 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
+	logger := log.New(os.Stdout, "morse-server: ", log.LstdFlags)
+	srv := server.NewServer(logger)
+	if err := srv.Start(); err != nil {
+		logger.Fatal(err)
+	}
 
 }
